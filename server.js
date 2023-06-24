de6bd9f7181f7d6692948f3de2e2d2cc5b8eb515
justinkwan1216 <justinkwan19971216@hotmail.com>
2023-06-25 02:36:43 +0800

    fixed port

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.post('/calculate', (req, res) => {
     }
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App listening on port ' + (process.env.PORT || 3000));
 });
